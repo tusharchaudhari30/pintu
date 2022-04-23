3b116be4310e9616947576437ef5fb73c4085f6d
tusharchaudhari30 <chaudharitushar2077@gmail.com>
2022-04-23 23:08:52 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
-
+#!/bin/bash
 sudo apt-get install gcc g++ python python-dev mercurial bzr gdb valgrind gsl-bin libgsl0-dev libgsl0ldbl flex bison tcpdump sqlite sqlite3 libsqlite3-dev libxml2 libxml2-dev libgtk2.0-0 libgtk2.0-dev uncrustify doxygen graphviz imagemagick texlive texlive-latex-extra texlive-generic-extra texlive-generic-recommended texinfo dia texlive texlive-latex-extra texlive-extra-utils texlive-generic-recommended texi2html python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev python-pygccxml
